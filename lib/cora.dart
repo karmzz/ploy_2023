9c41269f7a9e45eb94faf95b8427eb3ab405f4c0
Karina Ramírez <122934342+karmzz@users.noreply.github.com>
2023-05-16 16:26:51 -0600

    Create cora.dart

---
+++ lib/cora.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'cora_info.dart';
+
+
+class muestra_cora extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        home: Scaffold(
+            body: cora_info()
+        )
+    );
+  }
+
+}
